6277ecee8c7402930f8a443d16298b0a433049e6
rJ0711 <66914544+rJ0711@users.noreply.github.com>
2021-06-11 08:42:20 +0530

    Add files via upload

---
+++ java/com/rjrocks/covid_19analyzer/ChartOptions.java
@@ -0,0 +1,12 @@
+package com.rjrocks.covid_19analyzer;
+
+
+enum class Metric {
+    NEGATIVE, POSITIVE, DEATH
+}
+
+enum class TimeScale(val numDays: Int) {
+    WEEK(7),
+    MONTH(30),
+    MAX(-1)
+}
---
+++ java/com/rjrocks/covid_19analyzer/CovidData.kt
@@ -0,0 +1,12 @@
+package com.rjrocks.covid_19analyzer
+
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
+data class CovidData(
+    @SerializedName("dateChecked") val dateChecked: Date,
+    @SerializedName("positiveIncrease") val positiveIncrease: Int,
+    @SerializedName("negativeIncrease") val negativeIncrease: Int,
+    @SerializedName("deathIncrease") val deathIncrease: Int,
+    @SerializedName("state") val state: String
+)
